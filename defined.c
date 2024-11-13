ea80fb67dc2d19c2d01761bb49b276012f2c61cb
WoutVVD <184236120+WoutVVD@users.noreply.github.com>
2024-11-13 09:29:59 +0100

    define page

---
+++ defined.c
@@ -0,0 +1,6 @@
+#define ADDRESS     "tcp://192.168.1.23:1883"       // MQTT broker URL
+#define CLIENTID    "RaspberryPiClient"             // Unique client ID
+#define TOPIC       "Woutvvd"                       // MQTT topic
+#define PAYLOAD     "Hello from Raspberry Pi!"      // Message to send
+#define QOS         1                               // Quality of Service level
+#define TIMEOUT     10000L                          // Timeout in milliseconds
